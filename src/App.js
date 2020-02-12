714cc6925c1cb7609fd0b67d08ca0a515dd2172e
raj1020 <raj.1020@hotmail.com>
2020-02-12 23:03:37 +1100

    added dispatchers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 
-class App extends React.Component{
-
-  render () {
+function App () {
+    const counter = useSelector(state => state.counter);
+    const isLogged = useSelector(state => state.isLogged);
+  
     return (
-      <>
+      <div className = 'App'>
 
-      </>
+        <h1>Counter {counter}</h1>
+        <button>+</button>
+        <button>-</button>
+        {isLogged ? <h3>Valuable information I can see</h3> : ''}
+      </div>
 
     );
   }
-}
+
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import allReducers from './reducers';
+import {Provider} from 'react-redux';
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
